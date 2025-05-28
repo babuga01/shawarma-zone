c0b24f30ec055406b71fd4ace645df1bef527dfc
Mijiyawa <uthmanmijinyawa@gmail.com>
2025-05-28 16:31:50 +0100

    Update menu-grid.php to insert user session

--- app/views/menu-grid.php
+++ app/views/menu-grid.php
@@ -1,4 +1,10 @@
 <?php 
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: /public/login.php');
+    exit();
+}
 require_once __DIR__ . '/../../config/db.php';
 
 foreach ($dishes as $dish): 
