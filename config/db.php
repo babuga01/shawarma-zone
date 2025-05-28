491e2e90800133aedeb0e70a69bc172f354779ac
AbdulQudus Ayomide Abdulkareem <106139919+Ayomide4real@users.noreply.github.com>
2025-05-28 15:45:18 +0100

    connection to db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
-
+/* COnnection to Database */
 $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
